c6a10252274dbbb216694bc016b4ba5f7a53f515
Jen <jenniferanndcb@gmail.com>
2020-03-15 16:30:59 +0000

    banner component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PostcodeInput from "./components/PostcodeInput";
 import "./App.css";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <PostcodeInput />
     </div>
   );
---
+++ src/components/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Banner extends React.Component {
+  render() {
+    return <div className="Title">London Childhood Obesity 2018/19</div>;
+  }
+}
